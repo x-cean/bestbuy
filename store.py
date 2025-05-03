794042d93e9dc4015e9b5b016249bda0f27d16cd
xiao <194778728+x-cean@users.noreply.github.com>
2025-05-04 01:21:26 +0200

    updated class Store

--- store.py
+++ store.py
@@ -1,4 +1,5 @@
-from bestbuy.products import Product
+import products
+from products import Product
 
 
 class Store:
@@ -38,4 +39,40 @@ class Store:
             return [product for product in self.product_list if product.active]
 
     def order(self, shopping_list) -> float:
-        pass
+        if type(shopping_list) != list:
+            raise TypeError("Shopping list must be a list of tuple with Product objects and quantities.")
+        if not shopping_list:
+            raise ValueError("Shopping list cannot be empty.")
+        total_cost = 0
+        for product, quantity in shopping_list:
+            if type(product) != Product:
+                raise TypeError("All products must be of type Product.")
+            if type(quantity) != int:
+                raise TypeError("All quantities must be integers.")
+            if product not in self.product_list or product.quantity < quantity:
+                raise ValueError("We don't have enough of this product.")
+            total_cost += product.price * quantity
+            product.quantity -= quantity
+        return total_cost
+
+def test_store():
+    bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = products.Product("MacBook Air M2", price=1450, quantity=100)
+
+    best_buy = Store([bose, mac])
+    price = best_buy.order([(bose, 5), (mac, 30), (bose, 10)])
+    print(f"Order cost: {price} dollars.")
+
+def main():
+    product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    Product("Google Pixel 7", price=500, quantity=250),
+                    ]
+
+    best_buy = Store(product_list)
+    best_buy_products = best_buy.get_all_products()
+    print(best_buy.get_total_quantity())
+    print(best_buy.order([(best_buy_products[0], 1), (best_buy_products[1], 2)]))
+
+if __name__ == "__main__":
+    main()
