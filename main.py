f883c01e767e19f972500d107c00b518fd07adfa
xiao <194778728+x-cean@users.noreply.github.com>
2025-05-21 15:10:50 +0200

    started to work on main

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-MENU = """   Store Menu
+import products
+from products import Product
+import store
+from store import Store
+
+
+MENU = """   
+   Store Menu
    ----------
 1. List all products in store
 2. Show total amount in store
@@ -6,3 +13,15 @@ MENU = """   Store Menu
 4. Quit
 Please choose a number: """
 
+
+# set up initial stocks of inventory
+product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                products.Product("Google Pixel 7", price=500, quantity=250)
+                ]
+best_buy = store.Store(product_list)
+
+
+def start(a_store: Store):
+    print(MENU)
+start(best_buy)
