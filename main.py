105dd98039be39fa20983e3539350f342786720d
xiao <194778728+x-cean@users.noreply.github.com>
2025-05-21 15:05:44 +0200

    update Store with doc str and adjusted functions to call product method

---
+++ main.py
@@ -0,0 +1,8 @@
+MENU = """   Store Menu
+   ----------
+1. List all products in store
+2. Show total amount in store
+3. Make an order
+4. Quit
+Please choose a number: """
+
--- store.py
+++ store.py
@@ -4,6 +4,10 @@ from products import Product
 
 class Store:
     def __init__(self, product_list):
+        """
+        Initiator (constructor) method.
+        Each Store will have a list of Products
+        """
         if not product_list:
             raise ValueError("Store must have at least one product.")
         for product in product_list:
@@ -12,6 +16,9 @@ class Store:
         self.product_list = product_list
 
     def add_product(self, product):
+        """
+        Adds new Product
+        """
         if type(product) != Product:
             raise TypeError("All products must be of type Product.")
         if product in self.product_list:
@@ -19,6 +26,9 @@ class Store:
         self.product_list.append(product)
 
     def remove_product(self, product):
+        """
+        Removes a Product from Store.
+        """
         if type(product) != Product:
             raise TypeError("All products must be of type Product.")
         if product not in self.product_list:
@@ -26,6 +36,9 @@ class Store:
         self.product_list.remove(product)
 
     def get_total_quantity(self) -> int:
+        """
+        Returns how many items are in the store in total.
+        """
         if not self.product_list:
             return 0
         else:
@@ -33,12 +46,20 @@ class Store:
             return total_quantity
 
     def get_all_products(self) -> list:
+        """
+        Returns all products in the store that are active.
+        """
         if not self.product_list:
             return []
         else:
             return [product for product in self.product_list if product.active]
 
     def order(self, shopping_list) -> float:
+        """
+        Gets a list of tuples, where each tuple has 2 items:
+        Product (Product class) and quantity (int).
+        Buys the products and returns the total price of the order.
+        """
         if type(shopping_list) != list:
             raise TypeError("Shopping list must be a list of tuple with Product objects and quantities.")
         if not shopping_list:
@@ -51,11 +72,13 @@ class Store:
                 raise TypeError("All quantities must be integers.")
             if product not in self.product_list or product.quantity < quantity:
                 raise ValueError("We don't have enough of this product.")
-            total_cost += product.price * quantity
-            product.quantity -= quantity
+            total_cost += product.buy(quantity)
         return total_cost
 
 def test_store():
+    """
+    test from codio assignment 1
+    """
     bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
     mac = products.Product("MacBook Air M2", price=1450, quantity=100)
 
@@ -64,6 +87,9 @@ def test_store():
     print(f"Order cost: {price} dollars.")
 
 def main():
+    """
+    tests from codio assignment
+    """
     product_list = [Product("MacBook Air M2", price=1450, quantity=100),
                     Product("Bose QuietComfort Earbuds", price=250, quantity=500),
                     Product("Google Pixel 7", price=500, quantity=250),
@@ -73,6 +99,10 @@ def main():
     best_buy_products = best_buy.get_all_products()
     print(best_buy.get_total_quantity())
     print(best_buy.order([(best_buy_products[0], 1), (best_buy_products[1], 2)]))
+    print("New total: ", best_buy.get_total_quantity())
+    test_store()
+    print("New total: ", best_buy.get_total_quantity())
+
 
 if __name__ == "__main__":
     main()
