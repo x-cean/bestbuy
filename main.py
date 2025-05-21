6595bed908a9ba85563fab5645301fa86a4569c3
xiao <194778728+x-cean@users.noreply.github.com>
2025-05-21 16:33:49 +0200

    docstr

--- main.py
+++ main.py
@@ -23,16 +23,25 @@ best_buy = store.Store(product_list)
 
 
 def start(a_store: Store):
+    """
+    return a string of menu
+    """
     return MENU
 
 
 def create_product_dict(a_store: Store):
+    """
+    create a product dictionary from a given Store Object
+    """
     store_products = a_store.get_all_products()
     store_product_dict = {f"{i + 1}": store_products[i] for i in range(len(store_products))}
     return store_product_dict
 
 
 def list_products(a_store: Store):
+    """
+    print all products with information
+    """
     store_products = create_product_dict(a_store)
     print("------")
     for item_nr, item in store_products.items():
@@ -41,10 +50,18 @@ def list_products(a_store: Store):
 
 
 def show_total_amount(a_store: Store):
+    """
+    show total amount of all items in store
+    """
     print(f"Total items in store: {a_store.get_total_quantity()}")
 
 
 def make_an_order(a_store: Store):
+    """
+    make an order
+    buy the products (while stock quantity is enough)
+    returns a total cost of the order
+    """
     list_products(a_store)
     print("When you want to finish order, enter empty text.")
     product_dict = create_product_dict(a_store)
@@ -71,11 +88,20 @@ def make_an_order(a_store: Store):
 
 
 def quit_program(a_store: Store):
+    """
+    quit the program
+    Store not used but this simplifies how I call the function in main, so I kept it here
+    """
     print(f"Goodbye! We hope to see you again soon!")
     exit()
 
 
 def main():
+    """
+    main function
+    print menu
+    executes user selected function
+    """
     while True:
         user_input = input(start(best_buy))
 
